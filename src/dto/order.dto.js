dd377fda32a94cdada6b3c4c0b3843f5b223a4ab
Natasha <shotaoouji@gmail.com>
2025-09-28 21:50:03 -0300

    update: order dto

--- src/dto/order.dto.js
+++ src/dto/order.dto.js
@@ -1,10 +1,15 @@
-export function createOrderDto(body){
-  const {} = body ?? {};
+export function createOrderDto(body, user) {
+  const { email, first_name, last_name } = user ?? null;
+  if (!email || !first_name) throw new Error("Missing session 🧨");
+  const { code, items } = body ?? {};
+  if (!code) throw new Error("Missing data 💦");
+  const fullname = first_name.concat(last_name ?? "");
+  return { code, items, email, fullname };
+}
 
- }
-
-export function updateOrder(body){
+export function updateOrder(body) {
   const updOrder = {};
-  if(body?.orderId) updOrder.orderId = body.orderId
-  return updOrder
+  if (body?.items) updOrder.items = body.items;
+  if (body?.status) updOrder.status = body.status;
+  return updOrder;
 }
