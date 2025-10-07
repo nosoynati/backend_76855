a9dd9c5637181815e98d5869bce8b470b487581e
Natasha <shotaoouji@gmail.com>
2025-10-07 09:05:14 -0300

    add: mail router

--- src/routes/mail.router.js
+++ src/routes/mail.router.js
@@ -3,8 +3,8 @@ import { mailerController } from "../controllers/mail.controller.js";
 
 const mailRouter = Router();
 
-mailRouter.post('/api/mail/welcome',mailerController.sendWelcome);
-mailRouter.post('/api/mail/order-status',mailerController.sendorderStatus);
+mailRouter.post('/mail/welcome',mailerController.sendWelcome);
+mailRouter.post('/mail/order-status',mailerController.sendorderStatus);
 
 
 export default mailRouter;
